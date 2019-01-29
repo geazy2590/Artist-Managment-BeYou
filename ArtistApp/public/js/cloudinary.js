204e619673bde5f7057490a57b7f9baf92d4eba8
Gautam Rattihalli <gautamr2590@gmail.com>
2019-01-29 19:06:32 +0100

    Added image upload functionality

---
+++ ArtistApp/public/js/cloudinary.js
@@ -0,0 +1,8 @@
+var cloudinary = require('cloudinary');
+
+//Cloudinary configuration
+module.exports = cloudinary.config({ 
+    cloud_name: 'deuwergpo', 
+    api_key: '332151539923165', 
+    api_secret: 'HMGsmERYkU8RUoNsp_dxBgfxT_I' 
+});
